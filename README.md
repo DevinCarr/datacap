c13e792e0a7d92e4e040eb901d22ef922f56f500
devincarr <devin.carr0@gmail.com>
2015-12-12 20:48:38 -0600

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # datacap
 A simple Flask server that runs locally to check current data cap of Internet.
+
+## Running
+
+- Make sure to have Flask, Request, and Python 2.7 installed.
+- Run with `./start`
+
+This server will run on port `8080` locally and is configured with the normal Raspian settings.
+
+## Updating
+
+To update the internet cap, running `./update cap` will prompt for the current limit. This script can run on other machines as the webserver has a `/update` HTTP POST request URL that updates it.
+
+
---
+++ server.py
@@ -0,0 +1,34 @@
+import json
+import datetime
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# Entry point for application
+@app.route("/")
+def index():
+    file = open('data.json', 'r')
+    data = json.load(file)
+    file.close()
+    _monthlyPercentage = "{0:.1f}".format(datetime.date.today().day / 0.30)
+    _today = datetime.datetime.today().strftime("%I:%M%p %B %d %Y")
+    _date = data['time']
+    _percent = ("{0:.1f}").format(float(data['data'])/4.2)
+    _cap = ("{0:.1f}").format(float(data['data']))
+    _total = data['total']
+    return render_template('template.html', date=_date, percent=_percent, cap=_cap, total=TOTAL, today=_today, allowed=_monthlyPercentage)
+
+@app.route("/update", methods=['POST'])
+def get():
+    if request.method == 'POST':
+        req = request.get_json(force=True)
+        if req['data'] and req['time']:
+            with open('data.json', 'w') as file:
+                json.dump(req,file)
+                return '200: Successfully updated.'
+        else:
+            return 'Missing data'
+    else:
+        return 'Not a POST'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8080,debug=True)
