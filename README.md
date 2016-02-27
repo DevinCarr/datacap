6b77ca8daa8079aa2c3e2638e9b52e7f60b8b1f1
Devin Carr <me@devincarr.com>
2016-02-27 16:31:25 -0600

    Update update to be a url route.

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ A simple Flask server that runs locally to check current data cap of Internet.
 
 - Make sure to have Flask, Request, and Python 2.7 installed.
 - Run with `./start`
+- Start the datacap server with a cap amount by connecting with: `http://datacaphostname/update?cap=0`
 
 This server will run on port `8080` locally and is configured with the normal Raspian settings.
 
 ## Updating
 
-To update the internet cap, running `./update cap` will prompt for the current limit. This script can run on other machines as the webserver has a `/update` HTTP POST request URL that updates it.
+`host.com/update?cap=<value>` - Updates the cap and redirects to the homepage.
 
 
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import json
 import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 TOTAL = 450.0
+DATA = {'data': '', 'time': ''}
 
 # Entry point for application
 @app.route("/")
@@ -13,23 +14,28 @@ def index():
     file.close()
     _monthlyPercentage = "{0:.1f}".format(datetime.date.today().day / 0.30)
     _today = datetime.datetime.today().strftime("%I:%M%p %B %d %Y")
-    _date = data['time']
-    _percent = ("{0:.1f}").format(float(data['data'])/4.2)
-    _cap = ("{0:.1f}").format(float(data['data']))
+    _date = ''
+    if data['time']:
+        _date = data['time']
+    _percent = ''
+    _cap = ''
+    if data['data']:
+        _percent = ("{0:.1f}").format(float(data['data'])/4.2)
+        _cap = ("{0:.1f}").format(float(data['data']))
     return render_template('template.html', date=_date, percent=_percent, cap=_cap, total=TOTAL, today=_today, allowed=_monthlyPercentage)
 
-@app.route("/update", methods=['POST'])
-def get():
-    if request.method == 'POST':
-        req = request.get_json(force=True)
-        if req['data'] and req['time']:
-            with open('data.json', 'w') as file:
-                json.dump(req,file)
-                return '200: Successfully updated.'
-        else:
-            return 'Missing data'
+@app.route("/update")
+def update():
+    cap = request.args.get('cap')
+    if cap:
+        data = DATA
+        data['data'] = cap
+        data['time'] = datetime.datetime.today().strftime("%I:%M%p %B %d %Y")
+        with open('data.json', 'w') as file:
+            json.dump(data,file)
+            return redirect(url_for('index'))
     else:
-        return 'Not a POST'
+        return 'Missing data'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=8080)
